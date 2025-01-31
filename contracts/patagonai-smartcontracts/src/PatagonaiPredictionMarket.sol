897c3c1e4bcbaa24777264eefa0bf4b4e2d120ac
Gery P <geryp@Gerys-Laptop.home>
2025-01-31 20:30:51 +0100

    add necessary imports

---
+++ contracts/patagonai-smartcontracts/src/PatagonaiPredictionMarket.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {IERC20} from "@thirdweb-dev/contracts/eip/interface/IERC20.sol";
+import {Ownable} from "@thirdweb-dev/contracts/extension/Ownable.sol";
+import {ReentrancyGuard} from "@thirdweb-dev/contracts/external-deps/openzeppelin/security/ReentrancyGuard.sol";
+import "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
+import "@pythnetwork/pyth-sdk-solidity/PythStructs.sol";
+
+contract PatagonaiPredictionMarket is Ownable, ReentrancyGuard {
+    constructor() {}
+}
